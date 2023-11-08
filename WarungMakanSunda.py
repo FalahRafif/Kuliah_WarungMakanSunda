689d5f8931ff42b7d8534b78dc5083bdf3dc99f3
falahrafif <frthegamer95@gmail.com>
2023-11-08 19:53:32 +0700

    Add Menu Buat Pesanan

--- WarungMakanSunda.py
+++ WarungMakanSunda.py
@@ -5,12 +5,22 @@ import os
 namaKaryawan = ["Asep", "Irama", "Suetono"]
 passKaryawan = ["asep123", "iramacantik", "g30ppkn"]
 
-################ Property
+################ Data Login
 usernameLogin = ""
 passLogin = ""
-statusLogin = True
+statusLogin = False
+
+############### Data Menu
 pilihMenu = 0
 menuAktif = "login"
+
+############### Data Pesanan
+namaPembeli = []
+totalHargaPesanan = []
+banyakPesanan = []
+
+pesanan = []
+hargaPesanan = []
 
 ################ Function
 def MenuLogin() : 
@@ -29,10 +39,10 @@ def MenuLogin() :
     pilihMenu = int(input("Pilih Menu : "))
 
     # Cek Menu
-    if pilihMenu == 2 :
+    if pilihMenu == 2 : # Exit
         print("Terima kasih atas pekerjaan hari ini ~")
         exit()
-    elif pilihMenu == 1 :
+    elif pilihMenu == 1 : # Login
         os.system('cls')
         print("===================================================================")
         print("                 Warung Makan Sunda - Login Karyawan")
@@ -47,20 +57,109 @@ def MenuLogin() :
                     print("Login Berhasil")
                     statusLogin = True
                     menuAktif = "dashboard"
+                    return
             elif statusLogin == False and i == len(namaKaryawan) :
                 print("Username/Password tidak di temukan")
                 ctn = input("...")
                 os.system('cls')
+    else : 
+        print("Input Menu Tidak Valid!")
+        enter = input()
+        return
             
-
 def Dashboard(usernameLogin) :
+    global pilihMenu
+    global menuAktif
+    global statusLogin
+
     print("===================================================================")
     print("                 Warung Makan Sunda - Halo ", usernameLogin)
     print("===================================================================")
     print("[1] Buat Pesanan")
-    print("[2] Cek Pesanan")
+    print("[2] History Pesanan")
     print("[3] Logout")
+    pilihMenu = int(input("Pilih Menu : "))
 
+    if pilihMenu == 1 :
+        menuAktif = "buat-pesanan"
+        return
+    elif pilihMenu == 3 :
+        menuAktif = "login"
+        statusLogin = False
+        return
+    else :
+        print("Input Menu Tidak Valid!")
+        enter = input()
+        return
+
+def BuatPesanan(usernameLogin) :
+    global menuAktif 
+    pesanan.append([])
+    hargaPesanan.append([])
+    nomorPesanan = len(pesanan)-1
+    banyakPesanan.append([])
+    totalHargaPesanan.append(0)
+
+    print("===================================================================")
+    print("                 Warung Makan Sunda - Halo ", usernameLogin)
+    print("                     List Menu Makanan  & Minuman       ")
+    print("=========================================================================")
+    print("""|       Makanan       : Harga    |        Minuman           : Harga   |  
+| A. Nasi,Ayam Goreng : Rp.12000 | A. Es teh manis          : Rp.3000 |
+| B. Nasi,Telur Dadar : Rp.10000 | B. Es Jeruk              : Rp.5000 |
+| C. Nasi,Usus Goreng : Rp.8000  | C. Es Coklat             : Rp.4000 |
+=========================================================================""")
+    banyakBeli = int(input      ("Banyak Pesanan di Beli     :    "))
+    pembeli = str(         input("Nama Pembeli               :    "))
+    namaPembeli.append(pembeli)
+
+    # Validasi Pesanan
+    for i in range(banyakBeli) :
+        print                   ("=========== [Pesanan ke " + str(i+1) +"] ===========")
+        jenisPesanan = str(input(               "Jenis Pesanan (M/Mn)       :    "))
+        banyakPesanan[nomorPesanan].append(int(input("Banyak Pesanan             :    ")))
+
+        if jenisPesanan == "M" :  # Validasi Makanan
+            kodeMakanan = str(input("Pilih Kode Makanan (A/B/C) :    "))       
+            if kodeMakanan == "A" :
+                pesanan[nomorPesanan].append("Nasi,Ayam Goreng")
+                hargaPesanan[nomorPesanan].append(12000)
+                totalHargaPesanan[nomorPesanan] += 12000 * banyakPesanan[nomorPesanan][i]
+            elif kodeMakanan == "B" :
+                pesanan[nomorPesanan].append("Nasi,Telur Dadar")
+                hargaPesanan[nomorPesanan].append(10000)
+                totalHargaPesanan[nomorPesanan] += 10000 * banyakPesanan[nomorPesanan][i]
+            elif kodeMakanan == "C" :
+                pesanan[nomorPesanan].append("Nasi,Usus Goreng")
+                hargaPesanan[nomorPesanan].append(8000)
+                totalHargaPesanan[nomorPesanan] += 8000 * banyakPesanan[nomorPesanan][i]
+        elif jenisPesanan == "Mn" : # Validasi Minuman
+            kodeMakanan = str(input("Pilih Kode Makanan (A/B/C) :    "))       
+            if kodeMakanan == "A" :
+                pesanan[nomorPesanan].append("Es teh manis")
+                hargaPesanan[nomorPesanan].append(3000)
+                totalHargaPesanan[nomorPesanan] += 3000 * banyakPesanan[nomorPesanan][i]
+            elif kodeMakanan == "B" :
+                pesanan[nomorPesanan].append("Es Jeruk")
+                hargaPesanan[nomorPesanan].append(5000)
+                totalHargaPesanan[nomorPesanan] += 5000 * banyakPesanan[nomorPesanan][i]
+            elif kodeMakanan == "C" :
+                pesanan[nomorPesanan].append("Es Coklat")
+                hargaPesanan[nomorPesanan].append(4000)
+                totalHargaPesanan[nomorPesanan] += 4000 * banyakPesanan[nomorPesanan][i]
+        else : # Validasi Gagal
+            # Revert Pesanan
+            pesanan.pop(nomorPesanan)
+            hargaPesanan.pop(nomorPesanan)
+            totalHargaPesanan.pop(nomorPesanan)
+            namaPembeli.pop(nomorPesanan)
+            banyakPesanan.pop(nomorPesanan)
+
+            print("Kode Makanan Tidak Valid!")
+            a= input("...")
+            menuAktif = "dashboard"
+            return
+################ Looping Menu
 i = 1
 while i == 1 :
     os.system('cls')
@@ -70,6 +169,8 @@ while i == 1 :
             MenuLogin()
         case "dashboard" :
             Dashboard(usernameLogin)
+        case "buat-pesanan" :
+            BuatPesanan(usernameLogin)
 
 exit()
 
