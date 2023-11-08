90ffcef5744d6bc810e84ac291dfe5153a75ef91
falahrafif <frthegamer95@gmail.com>
2023-11-08 21:12:37 +0700

    Add Menu History Pesanan

--- WarungMakanSunda.py
+++ WarungMakanSunda.py
@@ -17,11 +17,11 @@ menuAktif = "login"
 ############### Data Pesanan
 namaPembeli = []
 totalHargaPesanan = []
-banyakPesanan = []
+uangDiBayar = []
 
 pesanan = []
 hargaPesanan = []
-
+banyakPesanan = []
 ################ Function
 def MenuLogin() : 
     global usernameLogin
@@ -60,11 +60,11 @@ def MenuLogin() :
                     return
             elif statusLogin == False and i == len(namaKaryawan) :
                 print("Username/Password tidak di temukan")
-                ctn = input("...")
+                enter = input("...")
                 os.system('cls')
     else : 
         print("Input Menu Tidak Valid!")
-        enter = input()
+        enter = input("...")
         return
             
 def Dashboard(usernameLogin) :
@@ -83,13 +83,16 @@ def Dashboard(usernameLogin) :
     if pilihMenu == 1 :
         menuAktif = "buat-pesanan"
         return
+    elif pilihMenu == 2 :
+        menuAktif = "history-pesanan"
+        return
     elif pilihMenu == 3 :
         menuAktif = "login"
         statusLogin = False
         return
     else :
         print("Input Menu Tidak Valid!")
-        enter = input()
+        enter = input("...")
         return
 
 def BuatPesanan(usernameLogin) :
@@ -159,6 +162,98 @@ def BuatPesanan(usernameLogin) :
             a= input("...")
             menuAktif = "dashboard"
             return
+
+    # Validasi Uang Bayar
+    print("Total Harga                :    Rp.", totalHargaPesanan[nomorPesanan])
+    uangBayar = int(input("Uang di Bayar              :    Rp. "))
+    if uangBayar < totalHargaPesanan[nomorPesanan] :
+        print("Uang Tidak Cukup!")
+        enter = input("...")
+        menuAktif = "dashboard"
+        return
+    else :
+        uangDiBayar.append(uangBayar)
+
+    BuatStruk(nomorPesanan+1)
+
+def BuatStruk(nomorPesanan = 0) :
+    global menuAktif
+    os.system("cls")
+
+    # Validasi nomor pesanan
+    try :
+        if nomorPesanan == 0 :
+            print("Nomor Pesanan Tidak Valid!")
+            enter = input("...")
+            menuAktif = "dashboard"
+            return
+        testPesanan = namaPembeli[nomorPesanan-1]
+        nomorPesanan = nomorPesanan - 1
+    except :
+        print("Nomor Pesanan Tidak Valid!")
+        enter = input("...")
+        menuAktif = "dashboard"
+        return
+    
+    # Buat Struk
+    print("===========================================")
+    print("===========S T R U K   B E L I ============")
+    print("===========================================")
+    print(" Kasir        : ", usernameLogin)
+    print(" Pembeli      : ", namaPembeli[nomorPesanan])
+    print(" --------------------------------------------")
+    for i in range(len(pesanan[nomorPesanan])) :
+        print(" %i. %ix - %s (Rp.%s) = Rp. %s" %(
+            (i+1) #nomor struk
+            , banyakPesanan[nomorPesanan][i] #banyak pesanan
+            , pesanan[nomorPesanan][i] #nama pesanan
+            , hargaPesanan[nomorPesanan][i] #harga per satuan
+            , (banyakPesanan[nomorPesanan][i] * hargaPesanan[nomorPesanan][i]) # Total Harga Pesanan
+        ))
+    print(" --------------------------------------------")
+    print(" Total Harga  : Rp.", totalHargaPesanan[nomorPesanan])
+    print(" Uang di Bayar: Rp.", uangDiBayar[nomorPesanan])
+    print(" Kembalian    : Rp.", (uangDiBayar[nomorPesanan] - totalHargaPesanan[nomorPesanan]))
+    print("=============================================")
+    print("=============================================")
+
+    print("")
+    print("Struk Tersimpan")
+    enter = input("...")
+    menuAktif = "dashboard"
+    return
+
+def HistoryPesanan(usernameLogin) :
+    global menuAktif
+    totalPesanan = len(namaPembeli)
+
+    print("===================================================================")
+    print("                 Warung Makan Sunda - Halo ", usernameLogin)
+    print("                            History Pesanan       ")
+    print("=========================================================================")
+    
+    for i in range(totalPesanan) :
+        print("%i. Pesanan Atas Nama '%s' - Total Harga : %i" %(
+            (i+1) #nomor history
+            , namaPembeli[i] #nama pembeli
+            , totalHargaPesanan[i] #total harga pesanan
+        ))
+    print("=========================================================================")
+    print("[1] Cek Detail/Buat Struk")
+    print("[2] Kembali")
+
+    pilihMenu = int(input("Pilih Menu : "))
+
+    if pilihMenu == 1 :
+        pilihMenu = int(input("Pilih Nomor History Pesanan : "))
+        BuatStruk(pilihMenu)
+    elif pilihMenu == 2 :
+        menuAktif = "dashboard"
+        return
+    else :
+        print("Input Menu Tidak Valid!")
+        enter = input("...")
+        return
 ################ Looping Menu
 i = 1
 while i == 1 :
@@ -171,110 +266,7 @@ while i == 1 :
             Dashboard(usernameLogin)
         case "buat-pesanan" :
             BuatPesanan(usernameLogin)
+        case "history-pesanan" :
+            HistoryPesanan(usernameLogin)
 
 exit()
-
-
-
-print("""
-    ===================================================================
- 
-                                        Warung Sunda
-                                List Menu Makanan dan Minuman 
-      
-    =====================================================================
-    :      Makanan      : Harga    :        Minuman           : Harga   :      
-    :A. Nasi,Ayam Goreng: Rp.12000 : A. Es teh manis          : Rp.3000 :
-    :B. Nasi,Telur dadar: Rp.10000 : B. Es Jeruk              : Rp.5000 :
-    :C. Nasi,Usus Goreng: Rp.8000  : C. Es Coklat             : Rp.4000 :
-    ====================================================================
-    """)
-pembeli = input("Masukan Nama Pembeli: ")
-print ("Nama Pembeli :", pembeli)
-
-total1=0
-jenis1=""
-porsi=0
-gelas=0
-
-def fungsimakanan():
-    global total1
-    global porsi
-    global jenis1
-    print("/~~Menu Makanan~~")
-    print("1. Nasi,Ayam Goreng - Rp12000")
-    print("2. Nasi,Telur dadar - Rp10000")
-    print("3. Nasi,Usus Goreng - Rp8000 ")
-    nomor=int(input("Masukan Pilihan:  "))
-    porsi=int(input("Berapa porsi   :  "))
-
-    if nomor==1:
-        total1=porsi*12000
-        print (porsi,"porsi Nasi,Ayam Goreng = Rp", total1)
-        jenis1=("Nasi,Ayam Goreng")
-    elif nomor==2:
-        total1=porsi*10000
-        print (porsi,"porsi Nasi,Telur dadar = Rp", total1)
-        jenis1=("Nasi,Telur dadar")
-    elif nomor==2:
-        total1=porsi*8000
-        print (porsi,"porsi Nasi,Usus Goreng = Rp", total1)
-        jenis1=("Nasi,Usus Goreng")
-    else:
-        print("Pilihan tifak ada")
-        fungsimakanan()
-
-
-fungsimakanan()
-
-total2=0
-jenis2=""
-
-def fungsiminuman():
-    global total2
-    global jenis2
-    global gelas
-    print("/n~~Menu Minuman~~")
-    print("1. Es teh manis          - Rp3000")
-    print("2. Es jeruk              - Rp5000")
-    print("3. Es Coklat             - Rp4000") 
-    nomor=int(input("Masukan Pilihan:"))
-    gelas=int(input("Berapa Gelas   :"))
-    
-    if nomor==1:
-        total2=gelas*3000
-        print(gelas," Es teh manis = Rp")
-        jenis2=(" Gelas Es teh manis")
-    elif nomor==2:
-        total2=gelas*5000
-        print(gelas," Es Jeruk")
-        jenis2=(" Gelas Es Jeruk")
-    elif nomor==3:
-        total2=gelas*4000
-        print(gelas," Es Coklat")
-        jenis2=(" Gelas Es Coklat")
-    else:
-        print("Pilihan tidak ada")
-        fungsiminuman
-
-
-fungsiminuman()
-
-totalsemua=0
-totalsemua=total1+total2
-print("/nTotal harus Dibayar: Rp",totalsemua)
-uang=int(input("Uang Tunai Pembeli: Rp."))
-kembalian=int(uang-totalsemua)
-print("Kembalian :",kembalian)
-
-print("/n===========================================")
-print("===========S T R U K   B E L I ============")
-print("===========================================")
-print (" Nama         :",pembeli)
-print (" Makanan      :",porsi,jenis1,"-",total1)
-print (" Minuman      :",gelas,jenis2,"-",total2)
-print (" Tagihan      : Rp.",totalsemua)
-print (" Uang         : Rp.",uang)
-print(" Kembalian     : Rp.",kembalian)
-print("=============================================")
-print("=============================================")
